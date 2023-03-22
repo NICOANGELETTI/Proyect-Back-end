c98359ee21d6bba0bc5645465fe1a8ac31c79545
= <nicocamilo2010@hotmail.com>
2023-03-21 23:42:57 -0300

    Realizamos deploy

--- src/main/java/com/portfolio/proyect/model/Experiencia.java
+++ src/main/java/com/portfolio/proyect/model/Experiencia.java
@@ -5,12 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
 
 
 @Entity
+@Table
 public class Experiencia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//Autoincrementales
